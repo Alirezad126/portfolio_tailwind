5e2f5855ae05c1d0fedf1fd6b78b4325e22112dd
alirezad126-at-957583705357 <alireza.daneshvar1377@gmail.com>
2024-05-21 15:56:45 -0400

    Updated CV

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -116,7 +116,7 @@ const Hero = () => {
 
             <a
               className="CVButton"
-              href="https://drive.google.com/file/d/1tLn4g8UAYrT2Wjla-2EpZTFCLjZwDbAq/view?usp=sharing"
+              href="https://drive.google.com/file/d/1Al1-c-bJb0yrMyUzkhkBqIThfx1rdISp/view?usp=sharing"
             >
               <motion.button className="heroButtons p-[20px] font-semibold text-base cursor-pointer border-2 rounded-full  bg-transparent 
           xl:p-[20px] sm:text-xl z-50" 
